ec6da81f054b5eeb045adf810f7fc58643355029
hungleung <hungleung.lee@me.com>
2022-09-08 13:50:18 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -11,19 +11,21 @@ DEVICE_TYPE?=router
 
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=\
-	luci-app-hd-idle \
-	luci-app-nfs \
-	luci-app-ssr-plus \
-	luci-app-ssr-plus_INCLUDE_SagerNet_Core \
-	luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client \
-	luci-app-transmission \
-	luci-app-udpxy \
-	luci-app-vlmcsd \
-	luci-app-wireguard \
-	luci-app-upnp \
-	luci-app-diskman \
-	luci-app-samba4 \
-	luci-app-minidlna
+	base-files \
+	ca-bundle \
+	dropbear \
+	fstools \
+	libc \
+	libgcc \
+	libustream-openssl \
+	logd \
+	mtd \
+	netifd \
+	opkg \
+	uci \
+	uclient-fetch \
+	urandom-seed \
+	urngd
 
 ifneq ($(CONFIG_SELINUX),)
 DEFAULT_PACKAGES+=busybox-selinux procd-selinux
@@ -40,11 +42,26 @@ DEFAULT_PACKAGES.nas:=\
 	lsblk \
 	mdadm
 # For router targets
-DEFAULT_PACKAGES.router:=
-
+DEFAULT_PACKAGES.router:=\
+	dnsmasq-full \
+	firewall \
+	iptables \
+	ppp \
+	ppp-mod-pppoe
 # For easy usage
-DEFAULT_PACKAGES.tweak:=
-
+DEFAULT_PACKAGES.tweak:=\
+	block-mount \
+	default-settings-chn \
+	kmod-ipt-raw \
+	kmod-nf-nathelper \
+	kmod-nf-nathelper-extra \
+	luci \
+	luci-app-filetransfer \
+	luci-app-turboacc \
+	luci-compat \
+	luci-lib-base \
+	luci-lib-fs \
+	luci-lib-ipkg
 
 ifneq ($(DUMP),)
   all: dumpinfo
